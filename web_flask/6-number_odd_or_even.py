09acd51ab2168225b65038787601af1e59f7589b
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-02 23:29:54 +0000

    Task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Runs a Flask web application /hbnb"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hbnb(text="is cool"):
+    """template
+    Returns:
+        [str]: [template]
+    """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Numbers template
+
+    Args:
+        n ([int]): [Number]
+
+    Returns:
+        [int]: [int template]
+    """
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """number template in html
+
+    Args:
+        n ([int]): [Number]
+
+    Returns:
+        [type]: [template]
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+if __name__ == '__main__':
+    app.run()
